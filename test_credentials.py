46266a416d806935a823515e3917a9e4e2b9397c
Daniel Krol <dan@alltuition.com>
2012-01-06 12:19:23 -0600

    [edulender] Better test credentials for testing. I think.

--- test_credentials.py
+++ test_credentials.py
@@ -1,6 +1,6 @@
 # These are credentials which are necessesary to run the test suite.
 # You should not put credentials for real merchants or processors here!
-processor_token = "69ac9c704329bb067d427bf0"
-merchant_key = "a1ebafb6da5238fb8a3ac9f6"
-merchant_password = "ae1aa640f6b735c4730fbb56"
+processor_token = "efafdf7dca3752a529519a41"
+merchant_key = "b2008122d36940d5c2a82421"
+merchant_password = "66baf3327c5239aa03290d63"
 
